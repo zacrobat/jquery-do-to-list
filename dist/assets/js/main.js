949739a649b83e0f2619958c8b54bbb02f5b1bf1
Zac Cobb <zacrobat55@gmail.com>
2016-08-09 11:55:11 +1000

    Add ability to remove list items on click

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -11,5 +11,7 @@ $(document).ready(function() {
     var toAdd = $('input[name=checkListItem]').val();
     $('.list').append('<div class="item">' + toAdd + '</div>');
   });
-
+  $(document).on('click', '.item', function() {
+    $(this).remove();
+  });
 });
--- src/js/main.js
+++ src/js/main.js
@@ -5,5 +5,7 @@ $(document).ready(function() {
     var toAdd = $('input[name=checkListItem]').val();
     $('.list').append('<div class="item">' + toAdd + '</div>');
   });
-
+  $(document).on('click', '.item', function() {
+    $(this).remove();
+  });
 });
